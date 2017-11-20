8510e8aa91c8857a1878568206bb18659113725f
kimkime2 <kkimmotti2@gmail.com>
2017-11-20 21:10:01 +0900

    Added a login page and change a little UI

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -16,13 +18,14 @@ def post_list(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-
     return render(request, 'blog/post_list.html', {'posts': posts,'form': form})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
