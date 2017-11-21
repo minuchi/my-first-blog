c450c24be542b509352916f661d609de0731a56b
kimkime2 <kkimmotti2@gmail.com>
2017-11-21 21:21:54 +0900

    Added replay and got lots of error and changed CSS

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post,Comment
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 from django.contrib.auth.decorators import login_required
 
-
+#포스트 리스트랑 게시글 작성 화면
 @login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -20,11 +20,24 @@ def post_list(request):
         form = PostForm()
     return render(request, 'blog/post_list.html', {'posts': posts,'form': form})
 
+#게시글 디테일 및 댓글 작성
 @login_required
 def post_detail(request, pk):
+    comments = Comment.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form, 'comments':comments})
 
+#게시글 수정화면
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -39,3 +52,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form,'post': post})
+
+#게시글 삭제
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect(url)
